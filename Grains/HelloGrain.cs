a60f465a52a32455c56b2a843e6270101c0c05a0
krb3d <Rumpl.ZoL@Gmail.com>
2021-02-04 23:09:46 +0200

    basic demo

---
+++ Grains/HelloGrain.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.Logging;
+using System.Threading.Tasks;
+
+namespace OrleansBasics
+{
+    public class HelloGrain : Orleans.Grain, IHello
+    {
+        private readonly ILogger _logger;
+
+        public HelloGrain(ILogger<HelloGrain> logger)
+        {
+            _logger = logger;
+        }
+
+        Task<string> IHello.SayHello(string greeting)
+        {
+            _logger.LogInformation(
+                "\n SayHello message received: greeting = '{greeting}'",
+                greeting);
+
+            return Task.FromResult($"\n Client said: '{greeting}', so HelloGrain says: Hello!");
+        }
+    }
+}
