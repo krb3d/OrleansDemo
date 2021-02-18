c43a42f4c00d548a07d1df872379d264ad3f2db6
Sergey Speshylov <s.speshylov@go-bbg.com>
2021-02-18 15:27:41 +0200

    Add factory & service registration extensions

---
+++ src/FileGrainStorage/FileGrainStorageFactory.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Orleans.Storage;
+using Microsoft.Extensions.Options;
+using Microsoft.Extensions.DependencyInjection;
+using Orleans.Configuration.Overrides;
+
+namespace GrainStorage
+{
+    public static class FileGrainStorageFactory
+    {
+        internal static IGrainStorage Create(IServiceProvider services, string name)
+        {
+            var optionsSnapshot = services.GetRequiredService<IOptionsSnapshot<FileGrainStorageOptions>>();
+            return ActivatorUtilities.CreateInstance<FileGrainStorage>(
+                    provider: services,
+                    name,
+                    optionsSnapshot.Get(name),
+                    services.GetProviderClusterOptions(name));
+        }
+    }
+}
---
+++ src/FileGrainStorage/FileSiloBuilderExtensions.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Orleans.Storage;
+using Microsoft.Extensions.DependencyInjection;
+using Orleans.Hosting;
+using Orleans.Runtime;
+using Orleans;
+
+namespace GrainStorage
+{
+    public static class FileSiloBuilderExtensions
+    {
+        public static ISiloHostBuilder AddFileGrainStorage(
+            this ISiloHostBuilder builder,
+            string providerName,
+            Action<FileGrainStorageOptions> options)
+        {
+            return builder.ConfigureServices(services => services.AddFileGrainStorage(providerName, options));
+        }
+
+        public static IServiceCollection AddFileGrainStorage(this IServiceCollection services, string providerName, Action<FileGrainStorageOptions> options)
+        {
+            services
+                .AddOptions<FileGrainStorageOptions>(providerName)
+                .Configure(options);
+
+            /*
+             * Our FileGrainStorage implements two interfaces, IGrainStorage and ILifecycleParticipant<ISiloLifecycle>
+             * therefore we need to register two named services for each interfaces
+             */
+
+            services
+                .AddSingletonNamedService(
+                    providerName,
+                    FileGrainStorageFactory.Create);
+
+            services
+                .AddSingletonNamedService(
+                    providerName,
+                    (serviceProvider, name) => (ILifecycleParticipant<ISiloLifecycle>)serviceProvider.GetRequiredServiceByName<IGrainStorage>(name));
+
+            return services;
+        }
+    }
+}
